9929c2c743548f0cef76d3830dd4d2680be2ed3f
Chatri Saenthamphol <chatri.stp97@gmail.com>
2026-09-14 15:29:50 +0700

    feat: add owner auth UI with Tailwind

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -6,6 +6,7 @@ export default createRouter({
     { path: '/', redirect: '/login' }, // dev1 can replace this with Landing Page
     { path: '/login', component: () => import('../views/LoginView.vue') },
     { path: '/register', component: () => import('../views/RegisterView.vue') },
+    { path: '/owner/profile', component: () => import('../views/OwnerProfileView.vue') },
     { path: '/sitter/profile', component: () => import('../views/SitterProfileView.vue') },
   ],
 })
